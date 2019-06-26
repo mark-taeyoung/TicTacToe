e523648e8f8442eb47fc9bd246890f98eb747e6a
Mark Kim <realtg2k@gmail.com>
2019-06-26 16:28:08 -0700

    marking works

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <vector>
+#include <random>
+#include <unordered_set>
 
 #define COL 3
 #define ROW 3
 
 
 bool checkInput (const char& input, const char board[ROW][COL]) {
+
     if (!isdigit(input)) {
         std::cout << "ERROR: input is NOT digit" << '\n';
         return false;
@@ -21,6 +23,35 @@ bool checkInput (const char& input, const char board[ROW][COL]) {
     return true;
 }
 
+
+void moveTurn (char board[ROW][COL], const int& goTo, const char& marker) {
+
+    int i = abs(goTo / COL - ROW) - 1;
+    int j = goTo % COL;
+
+    board[i][j] = marker;
+}
+
+void showBoard(char board[ROW][COL]) {
+
+    printf("\n\n"); 
+    printf("\t\t\t  %c | %c  | %c  \n", board[0][0], board[0][1], board[0][2]); 
+    printf("\t\t\t--------------\n"); 
+    printf("\t\t\t  %c | %c  | %c  \n", board[1][0], board[1][1], board[1][2]); 
+    printf("\t\t\t--------------\n"); 
+    printf("\t\t\t  %c | %c  | %c  \n\n", board[2][0], board[2][1], board[2][2]); 
+   
+    return;
+}
+
+
+int randNumGenerator(const int& lo, const int& hi) {
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::uniform_int_distribution<int> distribution(lo, hi);
+
+    return distribution(generator);
+}
 /*
 board 
 7 8 9
@@ -30,28 +61,66 @@ board
 
 int main(int argc, const char* argv[]) {
 
-    char board[ROW][COL] = {' '};
-    int count = 0;
+    char board[ROW][COL];
     char input = ' ';
 
+    char player;
+    char computer;
+    bool player_turn = true;
 
-    while(1) {
-        while(1) {
-            std::cout << "Enter your move: ";
-            std::cin >> input;
-            if (checkInput(input, board)) {
-                break;
-            }
-        }
-        int move = input - '0' - 1;
-        
-        int i = abs(move / COL - ROW) - 1;
-        int j = move % COL;
-    
-        std::cout << i << j << '\n';
-
-
+    std::unordered_set<size_t> available_slots;
+    for (size_t  i = 1; i <= ROW * COL; ++i) {
+        available_slots.insert(i);
     }
 
-    return 0;
+    std::cout << "Choose O or X: ";
+    std::cin >> player;
+
+    if (player == 'O') {
+        computer = 'X';
+    } else {
+        computer = 'O';
+        player_turn = false;
+    }
+
+    showBoard(board);
+    while(available_slots.size() > 0) {
+
+        char curr;
+        size_t target;
+
+        if(player_turn) {
+            std::cout << "Enter your move: ";
+            std::cin >> input;
+
+            if (!checkInput(input, board)) {
+                continue;
+            }
+            target = input - '0';
+            if (available_slots.find(target) == available_slots.end()) {
+                std::cout << "already filled\n";
+                continue;
+            }
+            curr = player;
+        } else {
+            curr = computer;
+            auto random_it = std::next(available_slots.begin(), randNumGenerator(0, available_slots.size() - 1));
+            target = *random_it;
+        }
+        
+        std::cout << "target : " << target << '\n';
+
+        moveTurn(board, target - 1, curr);
+        available_slots.erase(target);
+
+        player_turn = !player_turn;
+        
+        showBoard(board);
+        for (auto& x : available_slots)
+            std::cout << x;
+        std::cout << '\n';
+    }
+
+    std::cout << "GAME OVER\n";
+    std::exit(EXIT_SUCCESS);
 }
